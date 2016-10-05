44a4dd003afca0ae3cef6a52b476143f4cad2535
pulu <puluceno@gmail.com>
2016-10-05 19:03:15 -0300

    trying out ng-dialog

--- js/modules/app.js
+++ js/modules/app.js
@@ -1,23 +1,27 @@
 (function() {
 	var app = angular.module('caSearch', []);
-
 	app.controller('CaController', ['$scope', '$http', function($scope, $http) {
 		$scope.query = {};
 		$scope.cas = [];
 		this.submitForm = function() {
+			if ($scope.query.equipment != null)
+				$scope.query.equipment = $scope.query.equipment.toUpperCase();
 			$http({
 				url: "http://localhost:4567/ca",
 				method: "GET",
 				params: $scope.query
 			}).success(function(data) {
 				$scope.cas = data;
+				$scope.error = "";
+			}).error(function() {
+				$scope.error = "Aconteceu um erro."
 			});
-			//$http.get("http://localhost:4567/ca", $scope.query).
-
-			console.log($scope.cas);
-			console.log($scope.query);
 			$scope.query = {};
 			$scope.searchForm.$setPristine();
+		};
+		this.orderBy = function(field) {
+			$scope.orderCriteria = field;
+			$scope.orderDirection = !$scope.orderDirection;
 		};
 	}]);
 })();
