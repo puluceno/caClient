50e8851a7c3673c7bbffabfb64cee01c5ecf445d
pulu <puluceno@gmail.com>
2016-10-08 09:52:01 -0300

    finished modal

--- js/modules/app.js
+++ js/modules/app.js
@@ -1,46 +1,83 @@
 (function() {
-	var app = angular.module('caSearch', []);
+    var app = angular.module('caSearch', []);
 
-	app.controller('CaController', ['$scope', '$http', function($scope, $http) {
-		$scope.query = {};
-		$scope.cas = [];
-		$scope.fetching = false;
-		$scope.index=0;
+    app.controller('CaController', ['$scope', '$http', function($scope, $http) {
 
+        this.initialize = function() {
+            $scope.query = {};
+            $scope.query.status = {};
+            $scope.query.status.invalid = false;
+            $scope.query.status.valid = false;
+            $scope.cas = [];
+            $scope.fetching = false;
+            $scope.index = 0;
+        };
 
-		this.submitForm = function() {
-			$scope.fetching = true;
+        this.initialize();
 
-			if ($scope.query.equipment != null)
-				$scope.query.equipment = $scope.query.equipment.toUpperCase();
+        this.generatePDF = function(id) {
+            console.log(id);
+            $http({
+                url: "http://192.168.25.11:4567/ca/pdf",
+                method: "GET",
+								params: {'id':id}
+            }).success(function(data) {
+                $scope.error = "";
+                $scope.fetching = false;
+            }).error(function() {
+                $scope.error = "Aconteceu um erro!"
+                $scope.fetching = false;
+            });
+        };
 
-			$http({
-				url: "http://192.168.25.11:4567/ca",
-				method: "GET",
-				params: $scope.query
-			}).success(function(data) {
-				$scope.cas = data;
-				$scope.error = "";
-				$scope.fetching=false;
-			}).error(function() {
-				$scope.error = "Aconteceu um erro."
-				$scope.fetching=false;
-			});
+        this.submitForm = function() {
+            $scope.fetching = true;
 
-			console.log($scope.query);
-			$scope.query = {};
-			$scope.searchForm.$setPristine();
-		};
+            if ($scope.query.equipment != null)
+                $scope.query.equipment = $scope.query.equipment.toUpperCase();
+            if ($scope.query.company != null)
+                $scope.query.company = $scope.query.company.toUpperCase();
+            if ($scope.query.status.valid) {
+                $scope.query.status = "VALIDO";
+            } else if ($scope.query.status.invalid) {
+                $scope.query.status = "VENCIDO";
+            }
+            if (($scope.query.status.invalid && $scope.query.status.valid) || (!$scope.query.status.invalid && !$scope.query.status.valid)) {
+                $scope.query.status = null;
+            }
 
+            $http({
+                url: "http://192.168.25.11:4567/ca",
+                method: "GET",
+                params: $scope.query
+            }).success(function(data) {
+                $scope.cas = data;
+                $scope.error = "";
+                $scope.fetching = false;
+            }).error(function() {
+                $scope.error = "Aconteceu um erro!"
+                $scope.fetching = false;
+            });
 
-		this.orderBy = function(field) {
-			$scope.orderCriteria = field;
-			$scope.orderDirection = !$scope.orderDirection;
-		};
+            $scope.searchForm.$setPristine();
+            this.initialize();
+        };
 
-		this.clear = function(){
-			ca={};
-		};
+        this.orderBy = function(field) {
+            $scope.orderCriteria = field;
+            $scope.orderDirection = !$scope.orderDirection;
+        };
 
-	}]);
+        this.clear = function() {
+            ca = {};
+        };
+
+        app.directive("detailModal", function() {
+            return {
+                restrict: 'E',
+                templateUrl: 'detail-modal.html'
+            };
+        });
+
+    }]);
 })();
