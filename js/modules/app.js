5532e21002cd01e37fa3e73d0173887d9d555c18
pulu <puluceno@gmail.com>
2016-12-29 18:46:24 -0200

    95% stage 2

--- js/modules/app.js
+++ js/modules/app.js
@@ -1,23 +1,32 @@
-    var app = angular.module("caApp", ['ngTagsInput','ui.bootstrap']).config(function($sceDelegateProvider) {
+    var app = angular.module("caApp", ['ngTagsInput', 'ui.bootstrap', 'ngFileUpload']).config(function($sceDelegateProvider) {
         $sceDelegateProvider.resourceUrlWhitelist([
             // Allow same origin resource loads.
             'self',
-            'http://localhost:4567/*'
+            'http://localhost:4567/*',
+            'http://localhost:4567/durability',
+            '*'
         ]);
     });
 
-    app.directive('fileModel', ['$parse', function ($parse) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var model = $parse(attrs.fileModel);
-            var modelSetter = model.assign;
+    app.config(function(tagsInputConfigProvider) {
+        tagsInputConfigProvider.setDefaults('tagsInput', {
+            placeholder: ''
+        });
+    });
 
-            element.bind('change', function(){
-                scope.$apply(function(){
-                    modelSetter(scope, element[0].files[0]);
+
+    app.directive('fileModel', ['$parse', function($parse) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                var model = $parse(attrs.fileModel);
+                var modelSetter = model.assign;
+
+                element.bind('change', function() {
+                    scope.$apply(function() {
+                        modelSetter(scope, element[0].files[0]);
+                    });
                 });
-            });
-        }
-    };
-}]);
+            }
+        };
+    }]);
--- js/modules/controllers/durabilityController.js
+++ js/modules/controllers/durabilityController.js
@@ -1,30 +1,37 @@
-app.controller("durabilityController", ['$scope', '$http', function($scope, $http) {
+app.controller("durabilityController", ['$scope', '$http', 'Upload', '$timeout', function($scope, $http, Upload, $timeout) {
+    // var baseUrl = "http://perito2000.linkpc.net:4567/";
+    // var downloadUrl = "http://perito2000.linkpc.net:90/files/";
     var baseUrl = "http://localhost:4567/";
+    var downloadUrl = "http://localhost:8000/";
+    $scope.cont = {};
 
-    $scope.durabilities = [{
-        "date": "27/10/2016",
-        "equipment": "Luva cirúrgica",
-        "material": "plástico seco",
-        "days": "9999",
-        "fileName": "algum.pdf"
-    }, {
-        "date": "29/11/2016",
-        "equipment": "RESPIRADOR PURIFICADOR DE AR TIPO FILTRO QUÍMICO DE BAIXA CAPACIDADE",
-        "material": "látex",
-        "days": "45",
-        "fileName": "algum_arquivo_baixado_da_internet_como_base_de_conhecimento.pdf"
-    }];
+    this.getDurabilities = function() {
+        $http({
+            url: baseUrl + "durability",
+            method: "GET"
+        }).success(function(data) {
+            $scope.durabilities = data;
+        }).error(function() {
+            $scope.insertError = true;
+            $scope.errorMsg = "Aconteceu um erro!"
+            $timeout(function() {
+                $scope.error = false;
+            }, 10000);
+        });
+    };
 
     this.getEquipments = function() {
         $http({
             url: baseUrl + "equipment",
             method: "GET"
         }).success(function(data) {
-            $scope.fetching = false;
             $scope.equipments = data;
         }).error(function() {
-            $scope.error = "Aconteceu um erro!"
-            $scope.fetching = false;
+            $scope.insertError = true;
+            $scope.errorMsg = "Aconteceu um erro!"
+            $timeout(function() {
+                $scope.error = false;
+            }, 10000);
         });
     };
 
@@ -33,12 +40,91 @@ app.controller("durabilityController", ['$scope', '$http', function($scope, $htt
             url: baseUrl + "material",
             method: "GET"
         }).success(function(data) {
-            $scope.fetching = false;
             $scope.materials = data;
         }).error(function() {
-            $scope.error = "Aconteceu um erro!"
-            $scope.fetching = false;
+            $scope.insertError = true;
+            $scope.errorMsg = "Aconteceu um erro!"
+            $timeout(function() {
+                $scope.error = false;
+            }, 10000);
         });
+    };
+
+    this.init = function() {
+        $scope.newDurability = {};
+        this.getEquipments();
+        this.getMaterials();
+        this.getDurabilities();
+    };
+
+    this.init();
+
+    this.createNew = function(file) {
+        $scope.fetching = true;
+        var file = $scope.newDurability.file
+        var fd = new FormData();
+        fd.append("file", file);
+        fd.append("data", angular.toJson($scope.newDurability));
+        $http.post(baseUrl + "durability", fd, {
+            headers: {
+                'Content-Type': undefined
+            },
+            transformRequest: angular.identity,
+            params: {
+                fd
+            }
+        }).success(function(data) {
+            $scope.fetching = false;
+            $scope.durabilities = data;
+            $scope.success = true;
+            $scope.successMsg = "Durabilidade adicionada!"
+            $timeout(function() {
+                $scope.success = false;
+            }, 10000);
+        }).error(function(status) {
+            $scope.insertError = true;
+            $scope.errorMsg = "Aconteceu um erro!"
+            $scope.fetching = false;
+            $timeout(function() {
+                $scope.error = false;
+            }, 10000);
+        });
+        $scope.createDurabilityForm.$setPristine();
+        $scope.newDurability = {};
+        this.getMaterials();
+    };
+
+    this.update = function(durability) {
+        $scope.fetching = true;
+        var file = durability.file
+        var fd = new FormData();
+        fd.append("file", file);
+        fd.append("data", angular.toJson(durability));
+        $http.post(baseUrl + "durability", fd, {
+            headers: {
+                'Content-Type': undefined
+            },
+            transformRequest: angular.identity,
+            params: {
+                fd
+            }
+        }).then(function successCallback(data) {
+                $scope.durabilities = data;
+                $scope.fetching = false;
+                $scope.success = true;
+                $scope.successMsg = "Durabilidade atualizada!"
+                $timeout(function() {
+                    $scope.success = false;
+                }, 10000);
+            },
+            function errorCallback(status) {
+                $scope.error = true;
+                $scope.errorMsg = "Aconteceu um erro!"
+                $scope.fetching = false;
+                $timeout(function() {
+                    $scope.error = false;
+                }, 10000);
+            });
     };
 
     this.orderBy = function(field) {
@@ -46,7 +132,50 @@ app.controller("durabilityController", ['$scope', '$http', function($scope, $htt
         $scope.orderDirection = !$scope.orderDirection;
     };
 
-    this.getEquipments();
-    this.getMaterials();
+    this.delete = function(id) {
+        $scope.fetching = true;
+        $http({
+            url: baseUrl + "durability",
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            params: {
+                "id": id
+            }
+        }).success(function(data) {
+            $scope.durabilities = data;
+            $scope.success = true;
+            $scope.successMsg = "Durabilidade removida!"
+            $scope.fetching = false;
+            $timeout(function() {
+                $scope.success = false;
+            }, 10000);
+        }).error(function() {
+            $scope.insertError = true;
+            $scope.errorMsg = "Aconteceu um erro!"
+            $scope.fetching = false;
+            $timeout(function() {
+                $scope.error = false;
+            }, 10000);
+        });
+    };
+
+    this.getFile = function(fileName) {
+        return downloadUrl + fileName;
+    };
+
+    this.printMaterials = function(data) {
+        return data.map(function(elem) {
+            return elem.text;
+        }).join(", ");
+    };
+
+    $scope.loadMaterials = function($query) {
+        var materials = $scope.materials;
+        return materials.filter(function(materials) {
+            return materials.text.indexOf($query.toLowerCase()) != -1;
+        });
+    };
 
 }]);
